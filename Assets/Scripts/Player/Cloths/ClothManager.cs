786fece5e3950cbb42733ef996bd435c31bf5e5d
Pinkstrenhi <82385580+Pinkstrenhi@users.noreply.github.com>
2023-06-19 16:32:54 -0300

    Cloth Manager

---
+++ Assets/Scripts/Player/Cloths/ClothManager.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Core.Singleton;
+
+namespace Cloth
+{
+    public enum TypeCloth
+    {
+        ClothBasic,
+        ClothSpeed
+    }
+    public class ClothManager : Singleton<ClothManager>
+    {
+        public List<ClothSetup> clothSetups;
+
+        public ClothSetup GetSetupByType(TypeCloth typeCloth)
+        {
+            return clothSetups.Find(i => i.typeCloth == typeCloth);
+        }
+    }
+    [Serializable]
+    public class ClothSetup
+    {
+        public TypeCloth typeCloth;
+        public Texture2D texture2D;
+    }
+}
