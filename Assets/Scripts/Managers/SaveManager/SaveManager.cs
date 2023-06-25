fb9e1a74c1a146bd927fbf8dbea7712bf42ae4a2
Pinkstrenhi <82385580+Pinkstrenhi@users.noreply.github.com>
2023-06-25 20:40:40 -0300

    Save Game State

--- Assets/Scripts/Managers/SaveManager/SaveManager.cs
+++ Assets/Scripts/Managers/SaveManager/SaveManager.cs
@@ -4,33 +4,62 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using NaughtyAttributes;
-public class SaveManager : MonoBehaviour
+using Core.Singleton;
+public class SaveManager : Singleton<SaveManager>
 {
-    [Button]
-    private void Save()
+    private SaveSetup _saveSetup;
+    protected override void Awake()
     {
-        SaveSetup saveSetup = new SaveSetup();
-        saveSetup.lastLevel = 2;
-        saveSetup.playerName = "player";
-        var setupToJson = JsonUtility.ToJson(saveSetup,true);
-        Debug.Log(setupToJson);
-        SaveFile(setupToJson);
+        base.Awake();
+        _saveSetup = new SaveSetup();
+        _saveSetup.lastLevel = 2;
+        _saveSetup.playerName = "player";
     }
 
-    private void SaveFile(string json)
-    {
-        string path = Application.persistentDataPath + "/save.txt";
-        //string path = Application.streamingAssetsPath + "/save.txt";
-        
-        /*string fileLoaded = "";
-        if (File.Exists(path))
+    #region Save
+        [Button]
+        private void Save()
         {
-            fileLoaded = File.ReadAllText(path);
+            var setupToJson = JsonUtility.ToJson(_saveSetup,true);
+            Debug.Log(setupToJson);
+            SaveFile(setupToJson);
+        }
+        public void SaveName(string text)
+        {
+            _saveSetup.playerName = text;
+            Save();
+        }
+        public void SaveLastLevel(int level)
+        {
+            _saveSetup.lastLevel = level;
+            Save();
+        }
+
+        private void SaveFile(string json)
+        {
+            string path = Application.persistentDataPath + "/save.txt";
+            //string path = Application.streamingAssetsPath + "/save.txt";
+            
+            /*string fileLoaded = "";
+            if (File.Exists(path))
+            {
+                fileLoaded = File.ReadAllText(path);
+                File.WriteAllText(path,json);
+            }*/
+            Debug.Log(path);
             File.WriteAllText(path,json);
-        }*/
-        Debug.Log(path);
-        File.WriteAllText(path,json);
-    }
+        }
+        [Button]
+        private void SaveLevelOne()
+        {
+            SaveLastLevel(1);
+        } 
+        [Button]
+        private void SaveLevelFive()
+        {
+            SaveLastLevel(5);
+        }
+    #endregion
 }
 [Serializable]
 public class SaveSetup
@@ -38,3 +67,4 @@ public class SaveSetup
     public int lastLevel;
     public string playerName;
 }
+
